f8a7332148c4464dcb6e14d2f84af2076d5ef273
radiadus <radiadus@github.com>
2022-05-12 09:37:14 +0700

    auto push

--- push git.py
+++ push git.py
@@ -4,5 +4,5 @@ import os
 os.system("git add .")
 os.system("git commit -m \"auto push\"")
 os.system("git push")
-os.system("radiadus")
-os.system("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")
+# os.system("radiadus")
+# os.system("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")
