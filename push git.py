2cada713accec123f529c4ae53d18680083f6b61
radiadus <radiadus@github.com>
2022-05-12 09:33:57 +0700

    auto push

--- push git.py
+++ push git.py
@@ -1,8 +1,8 @@
 import os
 
-# os.system("cd testing_folder/Auto-push-test/")
+# os.system("cd Auto-push-test/")
 os.system("git add .")
 os.system("git commit -m \"auto push\"")
-os.system("git commit")
+os.system("git push")
 os.system("radiadus")
 os.system("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")
