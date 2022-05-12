033c4c44f30d513a23b39f9942917a1cc42d82be
radiadus <radiadus@github.com>
2022-05-12 13:34:05 +0700

    Testing Git Push

--- git-push.sh
+++ git-push.sh
@@ -1,12 +1,7 @@
 echo "Testing Git Push"
-read message
 git add .
 git commit -m"Testing Git Push"
-if [ -n "$(git status - porcelain)" ];
-then
- echo "IT IS CLEAN"
-else
- git status
- echo "Pushing data to remote server!!!"
- git push -u origin master
+git status
+echo "Pushing data to remote server!!!"
+git push 
 fi
